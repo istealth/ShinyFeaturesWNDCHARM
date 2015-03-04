d0f3a4dad79ae9c127823373ba4029b8d8cdae10
iStealth <stealth@nemo.local>
2015-03-04 18:25:58 +0100

    another library to read xls

--- tSNE-WND-CHARM.R
+++ tSNE-WND-CHARM.R
@@ -1,5 +1,9 @@
-require(gdata)
-df = read.xls ("/Users/stealth/GitHub/ShinyFeaturesWNDCHARM/crop_allSigFiles_Mm01.xls", sheet = 1, header = TRUE)
+#require(gdata)
+#df = read.xls ("/Users/stealth/GitHub/ShinyFeaturesWNDCHARM/TestWorkbook.xlsx", sheet = 1, header = TRUE)
+
+require(XLConnect)
+wb = loadWorkbook("/Users/stealth/GitHub/ShinyFeaturesWNDCHARM/crop_allSigFiles_Mm01.xls")
+df = readWorksheet(wb, sheet = "Hoja1", header = TRUE)
 
 colors = rainbow(length(unique(df$label)))
 names(colors) = unique(df$label)
