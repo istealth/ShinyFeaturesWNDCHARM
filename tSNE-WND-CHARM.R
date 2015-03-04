45aab5fee2e8c556eae310582266ff9bf950acc9
iStealth <stealth@nemo.local>
2015-03-04 15:46:18 +0100

    visualization using t-SNE

---
+++ tSNE-WND-CHARM.R
@@ -0,0 +1,12 @@
+require(gdata)
+df = read.xls ("/Users/stealth/GitHub/ShinyFeaturesWNDCHARM/crop_allSigFiles_Mm01.xls", sheet = 1, header = TRUE)
+
+colors = rainbow(length(unique(df$label)))
+names(colors) = unique(df$label)
+
+ecb = function(x,y){ 
+  plot(x,t='n'); 
+  text(x,labels=df$label, col=colors[df$label]) 
+}
+tsne_iris = tsne(df[,2:length(df[1,])], epoch_callback = ecb, perplexity=50)
+
